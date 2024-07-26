e1dc7812b8030cf5cef32818913724d7722cfea0
Diego Ronan Kulian <dkulian@veepee.com>
2024-07-26 14:43:36 +0200

    fix: remove remote route

--- apps/angular-shell/src/app/app.routes.ts
+++ apps/angular-shell/src/app/app.routes.ts
@@ -3,16 +3,6 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
   {
-    path: 'angular-remote-b',
-    loadChildren: () =>
-      import('angular-remote-b/Module').then((m) => m.RemoteEntryModule),
-  },
-  {
-    path: 'angular-remote-a',
-    loadChildren: () =>
-      import('angular-remote-a/Module').then((m) => m.RemoteEntryModule),
-  },
-  {
     path: '',
     component: NxWelcomeComponent,
   },
